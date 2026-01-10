342d60025a82c8d08f019b42fc2e986e5b00401d
MIkhsanDzul <sanexe24@gmail.com>
2026-01-10 14:09:45 +0700

    fix typo

--- app/core/Constants.php
+++ app/core/Constants.php
@@ -1,5 +1,5 @@
 <?php
-define('BASEURL', 'http://localhost/billesouth-boilerpalte/public');
+define('BASEURL', 'http://localhost/billesouth-boilerplate/public');
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
